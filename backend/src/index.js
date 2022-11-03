fd4d1b7a4dbafbe893558eb4c4864399155393b0
Bruno Moreira <comp.brunomoreira@gmail.com>
2022-11-03 02:31:40 -0300

    Adiciona método get de patrimonios

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const jsonParser = bodyParser.json()
+
+//mc855_db - user
+//grupoinventario - password
+
+// TODO: extrair conexão com banco para /backend/db/conn.js
+require('dotenv').config()
+const { MongoClient } = require('mongodb')
+const client = new MongoClient(process.env.ATLAS_URI)
+
+app.get('/', async (req, res) => {
+  await client.connect()
+  const db = client.db('test')
+  const collection = db.collection('patrimonios')
+  const results = await collection.find({}).toArray()
+  res.send(results)
+})
+
+// app.post('/', jsonParser, (req, res) => {})
+app.listen(3000)
