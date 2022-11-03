682d3e0ca5d8f98067c29ec57c6ec0a1b64cd27e
Guilherme <g.tetsuya13@gmail.com>
2022-11-03 11:08:09 -0300

    ajustes na conexao com o banco

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const url = 'localhost:3000/';
+const url = 'http://192.168.0.162:3000/';
 
-export const getData = ()=> axios.get(url);
+export const getData = () => axios.get(url + 'patrimonios');
 
 // export const createItem = (item)=>axios.post(url,item);
 
