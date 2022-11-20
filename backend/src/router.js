8c6fd029bbd1759a2efa277011ee13c89d1e8a84
f171509 <f171509@dac.unicamp.br>
2022-11-19 21:14:49 -0300

    Update router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -5,9 +5,12 @@ const PatrimonioController = require('./controllers/PatrimonioController')
 // GET
 router.get('/patrimonios', PatrimonioController.index)
 router.get('/patrimonios/:id', PatrimonioController.findById)
-router.get('/patrimonios/predio/:predio', PatrimonioController.findByPredio)
-router.get('/patrimonios/sala/:sala', PatrimonioController.findBySala)
-
+router.get('/patrimonios/imovel/:imovel', PatrimonioController.findByimovel)
+router.get('/patrimonios/sala/:localN1', PatrimonioController.findBylocalN1)
+router.get('/patrimonios/sala/:localN2', PatrimonioController.findBylocalN2)
+router.get('/patrimonios/sala/:localN3', PatrimonioController.findBylocalN3)
+router.get('/patrimonios/sala/:localN4', PatrimonioController.findBylocalN4)
+router.get('/patrimonios/sala/:localN5', PatrimonioController.findBylocalN5)
 // PUT
 router.put('/patrimonio/:id', PatrimonioController.updatePatrimonio)
 
