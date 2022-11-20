cc2b1ea0d009f1c86fdb14eb711c37084f069642
f171509 <f171509@dac.unicamp.br>
2022-11-19 21:11:26 -0300

    fix error

--- backend/src/controllers/PatrimonioController.js
+++ backend/src/controllers/PatrimonioController.js
@@ -24,13 +24,13 @@ module.exports = {
     return res.json(patrimonio)
   },
 
- async findBylocalN2(req, res) {
+  async findBylocalN2(req, res) {
     const localN2 = req.params.localN2
     patrimonio = await Patrimonio.find({ localN2: localN2 })
     return res.json(patrimonio)
   },
 
- async findBylocalN3(req, res) {
+  async findBylocalN3(req, res) {
     const localN3 = req.params.localN3
     patrimonio = await Patrimonio.find({ localN3: localN3 })
     return res.json(patrimonio)
@@ -40,14 +40,13 @@ module.exports = {
      const localN4 = req.params.localN4
      patrimonio = await Patrimonio.find({ localN4: localN4 })
      return res.json(patrimonio)
-   },
+  },
 
- async findBylocalN5(req, res) {
+  async findBylocalN5(req, res) {
     const localN5 = req.params.localN5
     patrimonio = await Patrimonio.find({ localN5: localN5 })
     return res.json(patrimonio)
   },
-
 
   async changeImovel(id, novoimovel) {
     await Patrimonio.findOneAndUpdate({ _id: id }, { imovel: novoimovel })
@@ -55,29 +54,34 @@ module.exports = {
 
   async changeLocalN1(id, novoN1) {
     await Patrimonio.findOneAndUpdate({ _id: id }, { localN1: novoN1 })
-  }
+  },
 
   async changeLocalN2(id, novoN2) {
     await Patrimonio.findOneAndUpdate({ _id: id }, { localN2: novoN2 })
-  }
+  },
 
   async changeLocalN3(id, novoN3) {
     await Patrimonio.findOneAndUpdate({ _id: id }, { localN3: novoN3 })
-  }
+  },
 
   async changeLocalN4(id, novoN4) {
     await Patrimonio.findOneAndUpdate({ _id: id }, { localN4: novoN4 })
-  }
+  },
 
   async changeLocalN5(id, novoN5) {
     await Patrimonio.findOneAndUpdate({ _id: id }, { localN5: novoN5 })
+  },
 
 
   async updatePatrimonio(req, res) {
     const id = req.params.id
     const area = req.body.areaPatrimonio
-    const predio = req.body.predio
-    const sala = req.body.sala
+    const imovel = req.body.imovel
+    const localN1 = req.body.localN1
+    const localN2 = req.body.localN2
+    const localN3 = req.body.localN3
+    const localN4 = req.body.localN4
+    const localN5 = req.body.localN5
     const tipo = req.body.tipo
     const descricao = req.body.descricao
     const marca = req.body.marca
