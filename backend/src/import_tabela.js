d432a35a3c016510162048c870130860c2e05ccf
f171509 <f171509@dac.unicamp.br>
2022-12-05 20:20:12 -0300

    Update import_tabela.js

--- backend/src/import_tabela.js
+++ backend/src/import_tabela.js
@@ -25,8 +25,8 @@ module.exports = {
 
         for (let i = 0; i < sheets.length; i++) {
           const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]])
-          temp.forEach((res) => {
-            data.push(res)
+          temp.forEach((resp) => {
+            data.push(resp)
           })
         }
         const db = mongoose.connection
